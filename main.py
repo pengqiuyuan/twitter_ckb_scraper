03989397c84d5d15b2d6f983c7066981abd95ff3
pengqiuyuan <pengqiyuanfj@idatage.com>
2023-05-14 12:48:41 +0800

    Update main.py

--- main.py
+++ main.py
@@ -8,10 +8,12 @@ path = pathlib.Path("./data/")
 path.mkdir(parents=True, exist_ok=True)
 
 since_date = str(datetime.datetime.today().date() - datetime.timedelta(days=1))
-json_file = path/str(datetime.datetime.today().date())
 
-def sns_scrape():
-    os.system(f'snscrape --jsonl --progress --since {since_date} twitter-search "新疆" > {json_file}.json')
+keywords = ["Tsinghua University", "Peking University", "Renmin University", "清华大学", "北京大学", "人民大学"]
+
+def sns_scrape(keyword):
+    json_file = path/(keyword + "_" + str(datetime.datetime.today().date()))
+    os.system(f'snscrape --jsonl --progress --since {since_date} twitter-search "{keyword}" > {json_file}.json')
 
     # 从 JSON 文件中读取数据
     with open(str(json_file) + ".json", "r", encoding="utf8") as file:
@@ -20,9 +22,15 @@ def sns_scrape():
     # 将数据转换为 DataFrame
     df = pd.json_normalize(data)
 
+    # 选择部分有用的列
+    useful_columns = ["url", "date", "content", "replyCount", "retweetCount", "likeCount", 
+                      "user.username", "user.displayname", "user.verified", "user.followersCount", 
+                      "user.friendsCount", "user.statusesCount"]
+    df = df[useful_columns]
+
     # 将 DataFrame 保存为 Excel 文件
     df.to_excel(str(json_file) + ".xlsx", index=False)
 
 if __name__ == "__main__":
-    sns_scrape()
-
+    for keyword in keywords:
+        sns_scrape(keyword)
